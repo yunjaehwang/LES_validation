360c7d443c6564fd29a59d653ebfb2be52e40310
yunjaeh <yunjaeh@stanford.edu>
2021-04-09 22:45:07 -0700

    update results and analysis code

---
+++ postProcess/pointCloud.py
@@ -0,0 +1,181 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 26 14:41:50 2021
+
+@author: yunjaeh
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+fCase = 'E1_20'
+# fPath = '../LES_ventilation/E1_5.base_inflow/output/PC/'
+fPath = '../LES_ventilation/'+fCase+'/output/PC/'
+
+coord = np.loadtxt(fPath+'C.pxyz', skiprows=1)
+
+
+dt, dstep = 0.0001, 100
+
+C0 = 100
+rawC, rawU, rawV, rawW, rawP = [], [], [], [], []
+
+steps     = np.arange(0,60001,dstep)
+timeSteps = steps*dt
+timeSteps = timeSteps-timeSteps[0]
+
+for i in steps:
+    print('Reading: ', fPath+'C.'+str(i).zfill(8)+'.pcd')
+    data=np.loadtxt(fPath+'C.'+str(i).zfill(8)+'.pcd',skiprows=1)
+    rawC.append(data[:,0])
+    rawU.append(data[:,1])
+    rawV.append(data[:,2])
+    rawW.append(data[:,3])
+    rawP.append(data[:,4])
+
+#%% choose only points inside the house and save data
+idx = np.asarray(rawC)[0,:] > 90.0
+XYZ = coord[idx,1:]
+C = np.asarray(rawC)[:,idx]/C0
+U = np.asarray(rawU)[:,idx]
+V = np.asarray(rawV)[:,idx]
+W = np.asarray(rawW)[:,idx]
+P = np.asarray(rawP)[:,idx]
+Umag = np.sqrt(U**2+V**2+W**2)
+
+[lenC, lenPt] = np.shape(C)
+
+np.savetxt('../Results/'+fCase+'_XYZ.txt',XYZ, delimiter=',', fmt='%4e')
+np.savetxt('../Results/'+fCase+'_U.txt',U, delimiter=',', fmt='%4e')
+np.savetxt('../Results/'+fCase+'_V.txt',V, delimiter=',', fmt='%4e')
+np.savetxt('../Results/'+fCase+'_W.txt',W, delimiter=',', fmt='%4e')
+np.savetxt('../Results/'+fCase+'_C.txt',C, delimiter=',', fmt='%4e')
+np.savetxt('../Results/'+fCase+'_P.txt',P, delimiter=',', fmt='%4e')
+
+
+#%%
+C_mean = np.mean(C,axis=0)
+plt.figure()
+plt.plot(C_mean,XYZ[:,1],'k.')
+# plt.xlim(21,31)
+# plt.xlim(16,21)
+# axes[0].set(xlabel='Time [sec]', ylabel='C/C0 [-]', \
+            # title='Time evolution of concentration')
+
+#%%
+idY0 = (XYZ[:,1] == 0.06)
+idZ0 = (XYZ[:,2] == 0.55)
+
+# idY1 = (XYZ_e1_5_opt[:,1] == 0.04)
+# idZ1 = (XYZ_e1_5_opt[:,2] == 0.55)
+
+
+plt.scatter(XYZ[idZ0,0], XYZ[idZ0,1], c=np.mean(U,axis=0)[idZ0])
+
+# fig, axes = plt.subplots(nrows=1, ncols=3)
+# axes[0].scatter(XYZ[idZ0,], XYZ[idZ0])
+# axes[1].scatter()
+# axes[2].scatter()
+
+#%%
+plt.plot(XYZ[idY0&idZ0,0], np.mean(U,axis=0)[idY0&idZ0]/6.6)
+# plt.plot(XYZ_e1_5_opt[idY1&idZ1,0], np.mean(U_e1_5_opt,axis=0)[idY1&idZ1]/6.6)
+# plt.title('E1_5, base_inflow')
+
+
+
+#%% age of air calculation and plot
+# aoa=[]
+# for i in range(1,lenC):
+    # aoa.append(np.trapz(C[0:i,:],dx=dt*dstep,axis=0))
+# aoa=np.asarray(aoa)
+
+aoa = np.trapz(C[0:i,:],dx=dt*dstep,axis=0)
+   
+# #%%
+# plt.figure()
+# for i in range(0,lenPt,100):
+#     plt.plot(timeSteps[1:], aoa[:,i],'gray')
+# plt.xlabel('Time [sec]')
+# plt.ylabel('Age of air [sec]')
+# plt.title('Time evolution of age of air at indoor locations')
+
+#%%
+plt.scatter(XYZ[idY0,0], XYZ[idY0,2],c=aoa[idY0])
+plt.colorbar()
+
+#%%
+plt.figure()
+# plt.hist(aoa,25, density=True)
+plt.hist(aoa,200, density=True)
+# plt.hist(aoa[-10,:],50, density=True)
+# plt.hist(aoa[-100,:],50, density=True)
+plt.xlabel('Age of air [sec]')
+plt.ylabel('Density')
+plt.title('PDF of age of air')
+
+#%% comparison between two1
+
+
+# %%
+
+# plt.figure()
+plt.hist(3600/aoa,density=True,bins=range(0,10000,100))
+# plt.xlabel('ACH [1/hr]')
+# plt.ylabel('Density')
+# plt.title('PDF of age of air')
+# plt.xlim(0,40)
+
+#%%
+# plt.figure()
+# plt.hist(aoa_E1_5,25,facecolor='blue',density=True, alpha=0.5)
+# plt.hist(aoa_A1_5,25,facecolor='red',density=True, alpha=0.5)
+# plt.hist(aoa_C1_5,25,facecolor='green',density=True, alpha=0.5)
+# plt.hist(aoa_D1_5,25,facecolor='yellow',density=True, alpha=0.5)
+
+
+
+#%% remaing Concentration
+fig, axes = plt.subplots(ncols=2, nrows=1)
+
+plt.figure()
+for i in range(0,lenPt,100):
+    axes[0].plot(timeSteps, C[:,i],'gray')
+axes[0].plot(timeSteps, np.mean(C,axis=1),'k')
+
+axes[0].set(xlabel='Time [sec]', ylabel='C/C0 [-]', \
+            title='Time evolution of concentration')
+
+axes[1].hist(C[-1,:])
+
+
+#%% read image
+data_fine    =np.loadtxt('../LES_ventilation/E1_5.fine/output/images_stats_old/mag.avg.raw_values.dat',skiprows=1);
+data_fine_new=np.loadtxt('../LES_ventilation/E1_5.fine/output/images_stats_new/mag.avg.raw_values.dat',skiprows=1);
+data_fine_all=np.loadtxt('../LES_ventilation/E1_5.fine/output/images_stats_all/mag.avg.raw_values.dat',skiprows=1);
+data_base    =np.loadtxt('../LES_ventilation/E1_5/output/images_stats/mag.avg.raw_values.dat',skiprows=1);
+data_inflow  =np.loadtxt('../LES_ventilation/E1_5.base_inflow/output/images_stats/mag.avg.raw_values.dat',skiprows=1);    
+
+# %%
+# plt.scatter(data_png[:,4], data_png[:,5], c=data_png[:,7])
+idY_base = (data_base[:,5] < 0.04) & (data_base[:,5] > 0.039)
+idY_fine = (data_fine[:,5] < 0.04) & (data_fine[:,5] > 0.039)
+idY_fine_new = (data_fine_new[:,5] < 0.04) & (data_fine_new[:,5] > 0.039)
+# idY_fine_all = (data_fine_all[:,5] < 0.04) & (data_fine_all[:,5] > 0.039)
+# idY_inflow = (data_inflow[:,5] < 0.041) & (data_inflow[:,5] > 0.039)
+
+plt.plot(data_base[idY_base,4], data_base[idY_base,7]/6.6,'k')
+plt.plot(data_fine[idY_fine,4], data_fine[idY_fine,7]/6.6,'r')
+plt.plot(data_fine_new[idY_fine_new,4], data_fine_new[idY_fine_new,7]/6.6,'g')
+# plt.plot(data_fine_all[idY_fine_all,4], data_fine_all[idY_fine_all,7]/6.6,'b')
+# plt.plot(data_inflow[idY_inflow,4], data_inflow[idY_inflow,7]/6.6,'b')
+plt.xlim(0.475, 0.625)
+# plt.plot(XYZ[idY0&idZ0,0], np.mean(Umag,axis=0)[idY0&idZ0]/6.6)
+
+
+
+
+
+
+
---
+++ postProcess/postProcess_figures.py
@@ -0,0 +1,267 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr  9 20:22:59 2021
+
+@author: yunjaeh
+"""
+
+#%% 
+import numpy as np
+import matplotlib.pyplot as plt
+
+#%%
+class fCase():
+    def __init__(self):
+        self.XYZ =[]
+        self.U = []
+        self.V = []
+        self.W = []
+        self.P = []
+        self.C = []
+        self.C0 = 100
+        self.AoA =[]
+        self.dt = 0.0001
+        self.dstep = 100
+        self.fPath =''
+    
+    def readCoord(self):
+        self.XYZ = np.loadtxt(self.fPath+'_XYZ.txt',delimiter=',')
+    def readVelocity(self):
+        self.U   = np.loadtxt(self.fPath+'_U.txt',delimiter=',')
+        self.V   = np.loadtxt(self.fPath+'_V.txt',delimiter=',')
+        self.W   = np.loadtxt(self.fPath+'_W.txt',delimiter=',')
+    def readP(self):
+        self.P   = np.loadtxt(self.fPath+'_P.txt',delimiter=',')
+    def readC(self):
+        self.C   = np.loadtxt(self.fPath+'_C.txt',delimiter=',')
+    def computeAoA(self):
+        self.AoA = np.trapz(self.C, dx=self.dt*self.dstep,axis=0)
+            
+
+#%%
+E1_5, E1_10, E1_20 = fCase(), fCase(), fCase()
+E1_5.fPath='../Results/E1_5'
+E1_5.readC()
+E1_5.computeAoA()
+
+E1_10.fPath='../Results/E1_10'
+E1_10.readC()
+E1_10.computeAoA()
+
+E1_20.fPath='../Results/E1_20'
+E1_20.readC()
+E1_20.computeAoA()
+
+E1_5_45=fCase()
+E1_5_45.fPath='../Results/E1_5.45'
+E1_5_45.readC()
+E1_5_45.computeAoA()
+
+E1_5_90=fCase()
+E1_5_90.fPath='../Results/E1_5.90'
+E1_5_90.readC()
+E1_5_90.computeAoA()
+
+#%%
+kwargs = dict(histtype='stepfilled', density=True, bins=100, \
+              color='b', edgecolor='b', alpha=0.3)
+
+plt.figure(figsize=(6,4))
+plt.hist(E1_5.AoA, **kwargs, label='Wall porosity =  5%')
+plt.hist(E1_10.AoA,hatch='/', **kwargs, label='Wall porosity = 10%')
+plt.hist(E1_20.AoA,hatch='+', **kwargs, label='Wall porosity = 20%')
+plt.xlabel('Age of air [second]')
+plt.ylabel('Probability')
+plt.xlim(0,1)
+plt.ylim(0,15)
+plt.legend()
+plt.savefig('../Results/E1_wall_porosity.png')
+    
+#%% E1, different angles
+kwargs = dict(histtype='stepfilled', density=True, bins=100, \
+              edgecolor='k',color='b')
+
+plt.figure(figsize=(6,4))
+plt.hist(E1_5.AoA,    alpha=0.3, **kwargs, label='Flow angle =  0$^\circ$')
+plt.hist(E1_5_45.AoA, alpha=0.5, **kwargs, label='Flow angle = 45$^\circ$')
+plt.hist(E1_5_90.AoA, alpha=0.9, **kwargs, label='Flow angle = 90$^\circ$')
+plt.xlabel('Age of air [second]')
+plt.ylabel('Probability')
+plt.xlim(0,2.5)
+plt.ylim(0,10)
+plt.legend()
+plt.title('Opening locations: center / center')
+plt.savefig('../Results/E1_flow_directions.png')
+
+#%%
+A1_5=fCase()
+A1_5.fPath='../Results/A1_5'
+A1_5.readC(),   A1_5.computeAoA()
+
+C1_5=fCase()
+C1_5.fPath='../Results/C1_5'
+C1_5.readC(),   C1_5.computeAoA()
+
+D1_5=fCase()
+D1_5.fPath='../Results/D1_5'
+D1_5.readC(),   D1_5.computeAoA()
+
+A1_5_45=fCase()
+A1_5_45.fPath='../Results/A1_5.45'
+A1_5_45.readC(),   A1_5_45.computeAoA()
+
+A1_5_90=fCase()
+A1_5_90.fPath='../Results/A1_5.90'
+A1_5_90.readC(),   A1_5_90.computeAoA()
+
+C1_5_45=fCase()
+C1_5_45.fPath='../Results/C1_5.45'
+C1_5_45.readC(),   C1_5_45.computeAoA()
+
+C1_5_90=fCase()
+C1_5_90.fPath='../Results/C1_5.90'
+C1_5_90.readC(),   C1_5_90.computeAoA()
+
+
+D1_5_45=fCase()
+D1_5_45.fPath='../Results/D1_5.45'
+D1_5_45.readC(),   D1_5_45.computeAoA()
+
+D1_5_90=fCase()
+D1_5_90.fPath='../Results/D1_5.90'
+D1_5_90.readC(),   D1_5_90.computeAoA()
+
+    
+#%%
+kwargs = dict(histtype='stepfilled', density=True, bins=40, \
+              edgecolor='k',alpha=0.3)
+
+plt.figure(figsize=(6,4))
+plt.hist(E1_5.AoA, color='b', **kwargs, label='Opening locations : center / center')
+plt.hist(A1_5.AoA, color='r', **kwargs, label='Opening locations : top / top')
+plt.hist(C1_5.AoA, color='g', **kwargs, label='Opening locations : bottom / bottom')
+plt.hist(D1_5.AoA, color='y', **kwargs, label='Opening locations : top / bottom')
+plt.xlabel('Age of air [second]')
+plt.ylabel('Probability')
+plt.xlim(0,1.)
+plt.ylim(0,10)
+plt.legend( bbox_to_anchor=(0.8,-0.2))
+plt.title('Wall porosity = 5%, flow angle = 0$^\circ$')
+plt.savefig('../Results/configurations_5%.png')
+
+#%% top / top
+kwargs = dict(histtype='stepfilled', density=True, bins=100, \
+              edgecolor='k',color='r')
+
+plt.figure(figsize=(6,4))
+plt.hist(A1_5.AoA,    alpha=0.3, **kwargs, label='Flow angle =  0$^\circ$')
+plt.hist(A1_5_45.AoA, alpha=0.5, **kwargs, label='Flow angle = 45$^\circ$')
+plt.hist(A1_5_90.AoA, alpha=0.9, **kwargs, label='Flow angle = 90$^\circ$')
+plt.xlabel('Age of air [second]')
+plt.ylabel('Probability')
+plt.xlim(0,2.5)
+plt.ylim(0,10)
+plt.legend()
+plt.title('Opening locations: top / top')
+plt.savefig('../Results/A1_flow_directions.png')
+
+#%% bottom / bottom 
+kwargs = dict(histtype='stepfilled', density=True, bins=100, \
+              edgecolor='k',color='g')
+
+plt.figure(figsize=(6,4))
+plt.hist(C1_5.AoA,    alpha=0.3, **kwargs, label='Flow angle =  0$^\circ$')
+plt.hist(C1_5_45.AoA, alpha=0.5, **kwargs, label='Flow angle = 45$^\circ$')
+plt.hist(C1_5_90.AoA, alpha=0.9, **kwargs, label='Flow angle = 90$^\circ$')
+plt.xlabel('Age of air [second]')
+plt.ylabel('Probability')
+plt.xlim(0,2.5)
+plt.ylim(0,10)
+plt.legend()
+plt.title('Opening locations: bottom / bottom')
+plt.savefig('../Results/C1_flow_directions.png')
+
+#%% top / bottom
+
+kwargs = dict(histtype='stepfilled', density=True, bins=100, \
+              edgecolor='k',color='y')
+
+plt.figure(figsize=(6,4))
+plt.hist(D1_5.AoA,    alpha=0.3, **kwargs, label='Flow angle =  0$^\circ$')
+plt.hist(D1_5_45.AoA, alpha=0.5, **kwargs, label='Flow angle = 45$^\circ$')
+plt.hist(D1_5_90.AoA, alpha=0.9, **kwargs, label='Flow angle = 90$^\circ$')
+plt.xlabel('Age of air [second]')
+plt.ylabel('Probability')
+plt.xlim(0,2.5)
+plt.ylim(0,10)
+plt.legend()
+plt.title('Opening locations: top / bottom')
+plt.savefig('../Results/D1_flow_directions.png')
+
+#%% 45 cases
+kwargs = dict(histtype='stepfilled', density=True, bins=100, \
+              edgecolor='k',alpha=0.5)
+
+plt.figure(figsize=(6,4))
+plt.hist(E1_5_45.AoA, color='b', **kwargs, label='Opening locations : center / center')
+plt.hist(A1_5_45.AoA, color='r', **kwargs, label='Opening locations : top / top')
+plt.hist(C1_5_45.AoA, color='g', **kwargs, label='Opening locations : bottom / bottom')
+plt.hist(D1_5_45.AoA, color='y', **kwargs, label='Opening locations : top / bottom')
+plt.xlabel('Age of air [second]')
+plt.ylabel('Probability')
+plt.xlim(0,1.2)
+plt.ylim(0,8)
+plt.legend( bbox_to_anchor=(0.8,-0.2))
+plt.title('Wall porosity = 5%, flow angle = 45$^\circ$')
+plt.savefig('../Results/configurations_45deg.png')
+
+#%% 90 degree
+kwargs = dict(histtype='stepfilled', density=True, bins=100, \
+              edgecolor='k',alpha=0.7)
+
+plt.figure(figsize=(6,4))
+plt.hist(E1_5_90.AoA, color='b', **kwargs, label='Opening locations : center / center')
+plt.hist(A1_5_90.AoA, color='r', **kwargs, label='Opening locations : top / top')
+plt.hist(C1_5_90.AoA, color='g', **kwargs, label='Opening locations : bottom / bottom')
+plt.hist(D1_5_90.AoA, color='y', **kwargs, label='Opening locations : top / bottom')
+plt.xlabel('Age of air [second]')
+plt.ylabel('Probability')
+plt.xlim(0,2.5)
+plt.ylim(0,4)
+plt.legend( bbox_to_anchor=(0.8,-0.2))
+plt.title('Wall porosity = 5%, flow angle = 90$^\circ$')
+plt.savefig('../Results/configurations_90deg.png')
+
+#%% print data
+
+print('E1', np.mean(E1_5.AoA), np.mean(E1_5_45.AoA), np.mean(E1_5_90.AoA))
+print('A1', np.mean(A1_5.AoA), np.mean(A1_5_45.AoA), np.mean(A1_5_90.AoA))
+print('C1', np.mean(C1_5.AoA), np.mean(C1_5_45.AoA), np.mean(C1_5_90.AoA))
+print('D1', np.mean(D1_5.AoA), np.mean(D1_5_45.AoA), np.mean(D1_5_90.AoA))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
